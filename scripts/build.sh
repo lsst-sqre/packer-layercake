792c4d442d7c816dc5f889f0490ba84f3ad0d4d2
Joshua Hoblitt <josh@hoblitt.com>
2015-11-09 10:46:48 -0700

    replace broken/stale version of build.sh
    with a known working version

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,18 @@
 set -o errexit
 
+ARGS=()
+
+if [ ! -z "$TAG" ]; then
+  ARGS+=('-t')
+  ARGS+=("$TAG")
+fi
+ARGS+=("$PRODUCTS")
+
 set -o verbose
 if grep -q -i "CentOS release 6" /etc/redhat-release; then
-    . /opt/rh/devtoolset-3/enable
+  . /opt/rh/devtoolset-3/enable
 fi
 set +o verbose
 
 source ./stack/loadLSST.bash
-eups distrib install -t w_2015_45 lsst_apps
+eups distrib install "${ARGS[@]}"
