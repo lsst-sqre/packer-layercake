a626b2f09bed42ca0676ce139512513da3439cb4
Joshua Hoblitt <josh@hoblitt.com>
2015-11-20 15:33:26 -0700

    enhance scripts/cleanup.sh per aws suggestions

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -11,3 +11,7 @@ fi
 # cleanup any ssh keys nova/cloud-init may have injected
 rm -rf /root/.ssh
 find  /home/ -maxdepth 2 -type d -name .ssh -exec rm -rf {} \;
+
+# per https://aws.amazon.com/articles/0155828273219400
+find /root/.*history /home/*/.*history -exec rm -f {} \; || true
+find /home/*/.*ssh -name "authorized_keys" –exec rm –f {} \; || true
