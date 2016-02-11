b9edcaed5c37bdad70444f86ee1438bea8e63de5
Joshua Hoblitt <josh@hoblitt.com>
2016-02-11 12:35:52 -0700

    cleanup /tmp & /var/tmp

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -19,3 +19,6 @@ find  /home/ -maxdepth 2 -type d -name .ssh -exec rm -rf {} \;
 # per https://aws.amazon.com/articles/0155828273219400
 find /root/.*history /home/*/.*history -exec rm -f {} \; || true
 find /home/*/.*ssh -name "authorized_keys" -exec rm -f {} \; || true
+
+# cleanup any junk left under /tmp & /var/tmp
+find /tmp /var/tmp -mindepth 1 -maxdepth 1 -print0 | xargs -0 rm -rf
